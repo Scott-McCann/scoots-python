5468cd502889c37137276aee258b255465099074
Scott McCann <scottmccann@live.com>
2016-04-19 10:15:54 -0500

    playing with nested blocks

---
+++ week1/hello_world.py
@@ -0,0 +1,16 @@
+print("Hello, world")
+
+if True is True:
+    myName = "Scott"
+    print("Howdy, "+ myName +"!")
+
+colors = ["red", "orange","yellow","green","blue","indigo","violet"]
+
+if "orange" in colors:
+    print("Orange is a color")
+    if "tan" in colors:
+        print("so is tan")
+    if "yellow" in colors:
+        print("And yellow too")
+
+        
