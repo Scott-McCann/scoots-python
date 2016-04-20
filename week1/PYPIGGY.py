39b12ad6ea025bc4a1af23588a539298e9e712b2
Scott McCann <scottmccann@live.com>
2016-04-20 15:57:43 -0500

    Pypiggy v0.1.1 conversion_word function defined, works.

--- week1/PYPIGGY.py
+++ week1/PYPIGGY.py
@@ -1,11 +1,75 @@
 cont='y'
 piglist=[]
+vow = ('a', 'e', 'i', 'o', 'u')
+def convert_word(word):
+    first = word[0]
+    if first in vow:
+        return word[1:] + "say"
+    else:
+        return word[1:] + word[0] + "ay"
+
+
+print("Hello and welcome to PYPIGGY v0.0.2")
 while cont is 'y':
-    print("Hello and welcome to PYPIGGY v0.0.2")
+
+
+
     aword= input("please enter a word: ")
-    newword = aword[1:]+ aword[0] +"ay"
-    piglist.append(newword)
+    pig= convert_word(aword)
+    piglist.append(pig)
+
     cont= input("continue?: ")
+
     if cont is "n":
-        break
-print(piglist)
+             continue
+
+for piggy in piglist:
+    print(piggy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     aword= input("please enter a word: ")
+#     if aword[0] == 'a':
+#         newword = aword[1:]+ aword[0]+ "say"
+#     elif aword[0] == 'e':
+#         newword = aword[1:]+ aword[0]+ "say"
+#     elif aword[0] == 'i':
+#         newword = aword[1:]+ aword[0]+ "say"
+#     elif aword[0] == 'o':
+#         newword = aword[1:]+ aword[0]+ "say"
+#     elif aword[0]== 'u':
+#         newword = aword[1:]+ aword[0]+ "say"
+#     else:
+#         newword = aword[1:]+ aword[0] +"ay"
+#
+#
+#
+#     piglist.append(newword)
+#     cont= input("continue?: ")
+#     if cont is "n":
+#         continue
+# for pig in (piglist):
+#         print(pig)
