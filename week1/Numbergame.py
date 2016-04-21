2acbe5372c333a569f6847d4a71266d3ec536f86
Scott McCann <scottmccann@live.com>
2016-04-21 16:45:29 -0500

    using while loop to control amount of tries. ValueError exception added to sanatize User Input.

--- week1/Numbergame.py
+++ week1/Numbergame.py
@@ -1,5 +1,5 @@
 import random
-def guess(int1=1,int2=1000):
+def pc_Guess(int1=1,int2=1000):
     """return a random integer from between two integers
     default values:(int1 = 1, int2 = 2)
     """
@@ -14,9 +14,26 @@ def secretmaker(int1=1, int2=1001):
 
     return secret
 
-f=secretmaker(1,1001)
-g=guess(1,1000)
+Secret = secretmaker(1,1001) #secret will be the number that must be guessed.
+tries = 5
+print(Secret)
+print("Hello and welcome to Number Game! ")
 
+while tries > 0:
+    #gives the user 5 tries, a correct guess breaks the loop. 
+        try:
+            user_Guess = int(input("please enter a number: "))
+            if user_Guess == Secret:
+                print("WOW!")
+                break
+            if user_Guess != Secret:
+                print("WRONG!")
+                tries = tries - 1
+        except ValueError:
+            print("Pretty sure I asked for a number. Enter Number: ")
 
-print(f)
-print(g)
+if tries > 0:
+    print("CONGRATULATIONS! You win!")
+
+if tries == 0:
+    print("GAME OVER")
