8411a0ad1169472c4ae284eb68128afa091bb0e1
Scott McCann <scottmccann@live.com>
2016-04-21 15:49:27 -0500

    defined two functions that return a random number between a range, they kind of do the same thing.

--- week1/Numbergame.py
+++ week1/Numbergame.py
@@ -1,5 +1,22 @@
 import random
-numbers = list(range(1,1000))
-secret = random.choice(numbers)
+def guess(int1=1,int2=1000):
+    """return a random integer from between two integers
+    default values:(int1 = 1, int2 = 2)
+    """
+    return random.randint(int1,int2)
 
-print(secret)
+
+def secretmaker(int1=1, int2=1001):
+    """creates a list of numbers, returns random
+    """
+    numbers = list(range(int1,int2))
+    secret = random.choice(numbers)
+
+    return secret
+
+f=secretmaker(1,1001)
+g=guess(1,1000)
+
+
+print(f)
+print(g)
