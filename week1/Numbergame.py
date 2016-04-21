53ee3b3282337e69a10e7f3ce888ebae39f58b7b
Scott McCann <scottmccann@live.com>
2016-04-21 16:57:20 -0500

    high and low evaluation. sorry this is devolping out of order.

--- week1/Numbergame.py
+++ week1/Numbergame.py
@@ -14,23 +14,27 @@ def secretmaker(int1=1, int2=1001):
 
     return secret
 
+user_Guess = 0
 Secret = secretmaker(1,1001) #secret will be the number that must be guessed.
 tries = 5
 print(Secret)
 print("Hello and welcome to Number Game! ")
 
 while tries > 0:
-    #gives the user 5 tries, a correct guess breaks the loop. 
-        try:
-            user_Guess = int(input("please enter a number: "))
-            if user_Guess == Secret:
+    #gives the user 5 tries, a correct guess breaks the loop.
+    try:
+        user_Guess = int(input("please enter a number: "))
+        if user_Guess == Secret:
                 print("WOW!")
                 break
-            if user_Guess != Secret:
-                print("WRONG!")
+        elif user_Guess > Secret:
+                print("Too High")
                 tries = tries - 1
-        except ValueError:
-            print("Pretty sure I asked for a number. Enter Number: ")
+        elif user_Guess < Secret:
+                print("Too Low")
+    except ValueError:
+        print("Pretty sure I asked for a number. Enter Number: ")
+
 
 if tries > 0:
     print("CONGRATULATIONS! You win!")
