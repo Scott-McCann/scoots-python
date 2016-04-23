c055c3a3001c1ec5a7c3e8268d4c00dd8923a89f
Scott McCann <scottmccann@live.com>
2016-04-23 11:04:28 -0500

    PYMAN.py

--- week1/PYMAN.py
+++ week1/PYMAN.py
@@ -1,9 +1,12 @@
 import random
 from random import choice
+#for some reason choice wouldn't import implicity
 
-def userGuess(guessList):
+
+def user_guess(guessList):
     while True:
-        """asks the user for a letter, checks if that letter is already in the guesslist, checks for correct input.
+        """asks the user for a letter, checks if that letter is already in the
+           guesslist, checks for correct input.
         """
         print("Please guess a letter: " )
         guess = input()
@@ -17,15 +20,17 @@ def userGuess(guessList):
         else:
             return guess
 
-def playAgain():
-    """ simply asks the user if they would like to play the game again.
-    """
+
+def play_again():
+    """ simply asks the user if they would like to play the game again."""
+
     print("Do you want to play again?")
     return input().lower().startswith('y')
 
-def getWord():
-    """picks a random word
-    """
+
+def get_word():
+    """picks a random word"""
+
     wordlist = []
     with open('/usr/share/dict/words', 'r') as f:
         for line in f:
@@ -37,7 +42,9 @@ def getWord():
 
 
 def blank(missed, correct, secretWord):
+
     print("Missed letters:", end = '')
+
     for letter in missed:
         print(letter)
     print()
@@ -51,11 +58,11 @@ def blank(missed, correct, secretWord):
     for letter in blanks:
         print(letter, end = '')
     print()
-
+tries = 6
 wordList = []
 missed = ''
 correct = ''
-secretWord = getWord()
+secretWord = get_word()
 wLength = len(secretWord)
 gameOver = False
 
@@ -63,10 +70,42 @@ print("Welcome to PYMAN")
 print("Do you want to play a game? ")
 print(secretWord)
 
-while gameOver == False:
+while gameOver is False :
     blank(missed, correct, secretWord)
 
-    guess = userGuess(missed + correct)
+    guess = user_guess(missed + correct)
 
     if guess in secretWord:
         correct = correct + guess
+
+        allCorrect = True
+        for alpha in range(len(secretWord)):
+            if secretWord[alpha] not in correct:
+                allCorrect = False
+                break
+
+        if allCorrect:
+            print("Thats Right! The word is "
+                 + secretWord
+                 + "! You Win! :D")
+            gameOver = True
+
+    else:
+        missed = missed + guess
+        tries = tries - 1
+
+        if tries == 0:
+            print("You have lost")
+            print("The secret word is : " + secretWord)
+            gameOver = True
+
+
+
+    if gameOver:
+        if play_again():
+            missed = ""
+            correct = ""
+            gameOver = False
+            secretWord = get_word()
+        else:
+           break
